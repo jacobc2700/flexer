063087327dc9b6a781fb56881cad93645ffbc78b
Jacob Chang <jacobchang124@gmail.com>
2023-02-15 19:06:43 -0600

    rebase-attempt-user-api

--- users/views.py
+++ users/views.py
@@ -49,7 +49,16 @@ def users(request):
             'last_name': last_name
         }
 
-        user = supabase.auth.sign_up(new_user_object)
+        auth_response = supabase.auth.sign_up(new_user_object)
+        # id, created at - properties we need generated from Supabase:
+        id_supabase = auth_response.user.id
+        created_at_supabase = auth_response.user.created_at.isoformat()
+
+        new_user_object['id'] = id_supabase
+        new_user_object['created_at'] = created_at_supabase
+        new_user_object['updated_at'] = created_at_supabase
+
+        # print(user['user_id'])
         data = supabase.table("users").insert(new_user_object).execute()
 
     return Response({'success': 'created a new user today'})
