353ebd536ab2516978025c52a643b158409498c0
Jacob Chang <jacobchang124@gmail.com>
2023-03-26 21:42:44 -0500

    finished basic company routes

--- docs/queries/views/companies.sql
+++ docs/queries/views/companies.sql
@@ -1,12 +1,9 @@
--- ...
+DROP VIEW IF EXISTS get_companies;
 
-DROP VIEW joined_tables;
-
-CREATE VIEW joined_tables AS (
+CREATE VIEW get_companies AS (
     SELECT companies.id, companies.company_name AS Name, COUNT(companies.id) AS Reviews, MAX(levels.base_salary) * 1000 AS Salary
     FROM levels
     JOIN companies
     ON levels.company_id = companies.id
     GROUP BY companies.id
-    LIMIT 5
 );
