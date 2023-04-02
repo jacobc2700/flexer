83418626534e331c689088af96d295c16537ff96
Jacob Chang <jacobchang124@gmail.com>
2023-04-02 18:51:36 -0500

    notes.py

--- server/companies/methods/notes.py
+++ server/companies/methods/notes.py
@@ -18,25 +18,15 @@ def get_notes(_request: HttpRequest, path_params=None) -> Response:
     Gets all notes associated with a company
     """
 
-    #TODO:
-    #TODO:
-    #TODO:
-    #TODO:
-    #TODO:
-    #TODO:
-    #TODO:
-    #TODO:
-    #TODO:
-    #TODO:
-
     try:
         company_name = None if "company_name" not in path_params else path_params["company_name"]
 
         if not company_name:
             return standard_resp(None, status.HTTP_400_BAD_REQUEST, "Missing company name.")
 
-        resp = supabase.table("").select(
-            "*").match({'user_id': user_id}).execute()
+        # resp = supabase.table("").select(
+        #     "*").match({'user_id': user_id}).execute()
+        resp = supabase.table("get_company_notes").select("*").eq("company_name", company_name).execute()
         return standard_resp(resp.data, status.HTTP_200_OK)
     except ValueError as err:
         return standard_resp(None, status.HTTP_400_BAD_REQUEST, str(err))
